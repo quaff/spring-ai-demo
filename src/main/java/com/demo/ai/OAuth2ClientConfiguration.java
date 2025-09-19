4b29e206fd0a7ac23d28e8bedcb3293bff18d903
Yanming Zhou <zhouyanming@gmail.com>
2025-09-19 10:03:22 +0800

    Polishing

--- src/main/java/com/demo/ai/OAuth2ClientConfiguration.java
+++ src/main/java/com/demo/ai/OAuth2ClientConfiguration.java
@@ -2,39 +2,32 @@ package com.demo.ai;
 
 import io.modelcontextprotocol.client.transport.customizer.McpSyncHttpClientRequestCustomizer;
 import io.modelcontextprotocol.common.McpTransportContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.ai.mcp.customizer.McpSyncClientCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.http.HttpHeaders;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.oauth2.client.*;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.web.context.request.RequestAttributes;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.reactive.function.client.*;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Supplier;
 
 @Configuration
 public class OAuth2ClientConfiguration {
 
-    private static final String CLIENT_CREDENTIALS_CLIENT_REGISTRATION_ID = "authserver-client-credentials";
-
-    private static final Authentication ANONYMOUS_AUTHENTICATION = new AnonymousAuthenticationToken("anonymous",
-            "anonymousUser", AuthorityUtils.createAuthorityList("ROLE_ANONYMOUS"));
-
-    @Bean
-    McpSyncClientCustomizer syncClientCustomizer() {
-        return (name, syncSpec) -> syncSpec.transportContextProvider(new AuthenticationMcpTransportContextProvider());
-    }
+    private Logger logger = LoggerFactory.getLogger(getClass());
 
     @Bean
     AuthorizedClientServiceOAuth2AuthorizedClientManager authorizedClientManager(ClientRegistrationRepository clientRegistrationRepository,
@@ -44,31 +37,52 @@ public class OAuth2ClientConfiguration {
     }
 
     @Bean
-    McpSyncHttpClientRequestCustomizer mcpSyncHttpClientRequestCustomizer(OAuth2AuthorizedClientManager authorizedClientManager) {
+    McpSyncClientCustomizer mcpSyncClientCustomizer() {
+        return (name, syncSpec) -> syncSpec.transportContextProvider(new AuthenticationMcpTransportContextProvider());
+    }
+
+    @Bean
+    McpSyncHttpClientRequestCustomizer mcpSyncHttpClientRequestCustomizer(OAuth2AuthorizedClientManager authorizedClientManager,
+                                                                          ClientRegistrationRepository clientRegistrationRepository) {
         return (builder, method, endpoint, body, context) -> {
             String accessToken;
-            if (contextInitialized.get()) {
+            if (contextInitialized) {
                 RequestAttributes requestAttributes = (RequestAttributes) context.get(AuthenticationMcpTransportContextProvider.REQUEST_ATTRIBUTES_KEY);
                 if (requestAttributes != null) {
                     accessToken = requestAttributes.getSessionId();
                 }
                 accessToken = "ak-34d4e32c85234139992fe713e8645b77"; // for test
             } else {
-                OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest.withClientRegistrationId(CLIENT_CREDENTIALS_CLIENT_REGISTRATION_ID)
-                        .principal(ANONYMOUS_AUTHENTICATION)
+                OAuth2AuthorizeRequest authorizeRequest = OAuth2AuthorizeRequest.withClientRegistrationId(findUniqueClientRegistration(clientRegistrationRepository))
+                        .principal("mcp-client")
                         .build();
                 accessToken = authorizedClientManager.authorize(authorizeRequest).getAccessToken().getTokenValue();
             }
-            builder.header("Authorization", "Bearer " + accessToken);
+            logger.info("Using access token {} for {} to {} with\n{}", accessToken, method, endpoint, body);
+            builder.header(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
         };
+    }
+
+    private static String findUniqueClientRegistration(ClientRegistrationRepository clientRegistrationRepository) {
+        String registrationId;
+        if (!(clientRegistrationRepository instanceof InMemoryClientRegistrationRepository repo)) {
+            throw new IllegalStateException("Expected an InMemoryClientRegistrationRepository");
+        }
+        var iterator = repo.iterator();
+        var firstRegistration = iterator.next();
+        if (iterator.hasNext()) {
+            throw new IllegalStateException("Expected a single Client Registration");
+        }
+        registrationId = firstRegistration.getRegistrationId();
+        return registrationId;
     }
 
     @EventListener
     void listen(ContextRefreshedEvent contextRefreshedEvent) {
-        contextInitialized.compareAndSet(false, true);
+        contextInitialized = true;
     }
 
-    private static final AtomicBoolean contextInitialized = new AtomicBoolean();
+    private boolean contextInitialized;
 
     static class AuthenticationMcpTransportContextProvider implements Supplier<McpTransportContext> {
 
